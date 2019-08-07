bb61e5892d948082784cfc9c69173a4c838a50e5
ailyntang <ailyntang@gmail.com>
2019-08-07 12:46:26 +1000

    Add new `Puzzle` class
    Includes constructor, `get rows` and `get columns`

--- puzzle.js
+++ puzzle.js
@@ -2,7 +2,7 @@ class Puzzle {
   constructor(input) {
     if (input === undefined) {
       const row = [0,0,0,0,0,0,0,0,0];
-      this.puzzle = [row, row, row, row, row, row, row, row, row];  
+      this.puzzle = [row, row, row, row, row, row, row, row, row];
     } else {
       this.puzzle = input;
     }
@@ -12,12 +12,15 @@ class Puzzle {
     return this.puzzle;
   }
 
+  /**
+   * Takes an array of rows (e.g. [[1, 2], [3, 4]])
+   * Returns an array of columns (e.g. [[1, 3], [2, 4]])
+  */
   get columns() {
-    return this.puzzle;
+    // First `map` cycles through the number of columns as an index `i`.
+    // Second `map` cycles picks out the correct number in each row to insert into the column.
+    return this.puzzle[0].map((_, i) => this.puzzle.map(row => row[i]));
   }
 }
 
 module.exports = Puzzle;
-
-const puzzle = new Puzzle([4,3]);
-console.log(puzzle.rows);
