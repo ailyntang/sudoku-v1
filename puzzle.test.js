cb1862bf49835e8543bb96d43ac92efd8299dd25
ailyntang <ailyntang@gmail.com>
2019-08-07 12:01:59 +1000

    Add basic tests for row, column, number, empty

--- puzzle.test.js
+++ puzzle.test.js
@@ -1,9 +1,46 @@
 const Puzzle = require('./puzzle');
 
 describe('an empty puzzle', () => {
-  let puzzle = new Puzzle;
+  let puzzle = new Puzzle();
 
   it('should have nine rows', () => {
-    expect(puzzle.rows.length).toBe(9);
+    expect(puzzle.rows).toHaveLength(9);
+  });
+
+  it('should have nine columns', () => {
+    expect(puzzle.columns).toHaveLength(9);
+  });
+
+  it('should have zeros for every number', () => {
+    puzzle.rows.forEach((row) => {
+      row.forEach((num) => {
+        expect(num).toBe(0);
+      });
+    });
+  });
+});
+
+describe('a row or column', () => {
+  const puzzle = new Puzzle;
+  const row = puzzle.rows[0];
+  const col = puzzle.columns[0];
+
+  it('should have nine numbers', () => {
+    expect(row).toHaveLength(9);
+    expect(col).toHaveLength(9);
+  });
+});
+
+describe('a number', () => {
+  const puzzle = new Puzzle;
+
+  it('should be an integer between 0 and 9 inclusive', () => {
+    const validNumbers = [0,1,2,3,4,5,6,7,8,9];    
+
+    puzzle.rows.forEach((row) => {
+      row.forEach((num) => {
+        expect(validNumbers).toContain(num);
+      });
+    });
   });
 });
